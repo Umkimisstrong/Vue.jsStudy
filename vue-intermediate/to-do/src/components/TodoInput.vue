d2ca8ac6823663bdf90b23e59fef68a09acd3dd7
KIM SANG KI <ckacl0233@gmail.com>
2023-02-22 16:06:09 +0900

    Vue Study Commit
    Vue Study Commit

--- vue-intermediate/to-do/src/components/TodoInput.vue
+++ vue-intermediate/to-do/src/components/TodoInput.vue
@@ -1,8 +1,12 @@
 <template>
     <!-- TodoInput -->
-    <div>
-        <input type="text" v-model="newTodoItem">
-        <button v-on:click="addTodo">add</button>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <button class="addBtn">add</button>
+        </span>
+
+
     </div>
 </template>
 
@@ -20,6 +24,9 @@ export default {
             // 저장하는 로직
             // 개발자도구의 애플리케이션 - 로컬스토리지에 key / value 로 저장이 된다.
              localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.clearInput();
+        },
+        clearInput : function () {
             this.newTodoItem = '';
         }
    },
@@ -28,5 +35,30 @@ export default {
 </script>
 
 <style scoped>
+
+input:focus {
+    outline : none;
+}
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input {
+    border-style:none;
+    font-size : 0.9rem;
+}
+.addContainer {
+    float : right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+    vertical-align: middle;
+}
 
 </style>
