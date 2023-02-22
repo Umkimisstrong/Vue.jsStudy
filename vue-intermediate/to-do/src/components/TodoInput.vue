e4bdd22bdcffcac4316e61dce897b7b07883f263
KIM SANG KI <ckacl0233@gmail.com>
2023-02-22 13:57:59 +0900

    Update TodoInput.vue

--- vue-intermediate/to-do/src/components/TodoInput.vue
+++ vue-intermediate/to-do/src/components/TodoInput.vue
@@ -1,12 +1,29 @@
 <template>
     <!-- TodoInput -->
     <div>
-        <input type="text">
+        <input type="text" v-model="newTodoItem">
+        <button v-on:click="addTodo">add</button>
     </div>
 </template>
 
 <script>
+export default {
+    data : function ()  {
+        return  {
+            newTodoItem: ""
+        }
+    },
+    methods: {
 
+        addTodo : function (){
+            //console.log(this.newTodoItem);
+            // 저장하는 로직
+            // 개발자도구의 애플리케이션 - 로컬스토리지에 key / value 로 저장이 된다.
+             localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.newTodoItem = '';
+        }
+   },
+}
 
 </script>
 
